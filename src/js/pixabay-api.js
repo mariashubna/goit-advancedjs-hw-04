518cf45460cfcf5e48dae442a04811b229eb50d7
mariashubna <mariashubna@gmail.com>
2024-10-19 16:07:31 +0200

    /

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,19 +4,17 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '39901627-719ccf8971235dd54bb900542';
 
 async function getImages({ page, perPage, query }) {
-  const {data} = await axios.get('',
-    {
-      params: {
-        key: API_KEY,
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page,
-        per_page: perPage,
-      },
-    }
-  );
+  const { data } = await axios.get('', {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page,
+      per_page: perPage,
+    },
+  });
   return data;
 }
 
